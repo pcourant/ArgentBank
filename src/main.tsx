7457127bd23babc46a393339bec7a22a5ab50f37
Pierre COURANT <pcourant.dev@gmail.com>
2022-10-17 13:55:32 +0200

    Add Router and Home page

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider, Route } from 'react-router-dom';
 
 import 'normalize.css';
 
-import App from './App';
-import './index.css';
+import './main.css';
+import Root from './routes/Root';
+import ErrorPage from './error-page';
+// import Contact from './routes/contact';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Root />,
+        errorElement: <ErrorPage />,
+        // children: [
+        //     {
+        //         path: 'contacts/:contactId',
+        //         element: <Contact />,
+        //     },
+        // ],
+    },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <App />
+        <RouterProvider router={router} />
     </React.StrictMode>,
 );
---
+++ src/routes/Root/index.tsx
@@ -0,0 +1,15 @@
+import { Outlet, Link } from 'react-router-dom';
+import Footer from './components/Footer';
+import Header from './components/Header';
+
+const Root = () => {
+    return (
+        <>
+            <Header />
+            <Outlet />
+            <Footer />
+        </>
+    );
+};
+
+export default Root;
