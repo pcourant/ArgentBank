2a109ff8ca1867c952dd143660c3993dbd5148d1
Pierre COURANT <pcourant.dev@gmail.com>
2022-10-29 14:47:28 +0200

    Remove UserContext

--- src/pages/App/index.tsx
+++ src/pages/App/index.tsx
@@ -2,16 +2,12 @@ import { Outlet } from 'react-router-dom';
 import Footer from './components/Footer';
 import Header from './components/Header';
 
-import { UserContextProvider } from '@utils/context';
-
 const App = () => {
   return (
     <>
-      <UserContextProvider>
-        <Header />
-        <Outlet />
-        <Footer />
-      </UserContextProvider>
+      <Header />
+      <Outlet />
+      <Footer />
     </>
   );
 };
